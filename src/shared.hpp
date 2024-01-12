0ce24c4263630fed5cb2d8f7281dfd74578f9f9d
mooooonk <moooookrui@gmail.com>
2024-01-12 15:29:53 +0800

    + passed extra 2, with extra 4 & 7 remaining

--- src/shared.hpp
+++ src/shared.hpp
@@ -1,15 +1,17 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
+
+/*
 #include <iostream>
 
 template <typename T>
 class SharedPtr
 {
 private:
-/*
+
     @attr ptr(pointer to T): a pointer to the given position
     @attr count(pointer to int): the number of instances pointing to a same place
-*/
+
     T *ptr;
     int *count;
 public:
@@ -81,4 +83,92 @@ SharedPtr<T> make_shared(Args&&... args)
 {
     return SharedPtr(new T(std :: forward<Args>(args) ...));
 }
+
+*/
+
+//Some problems happened when transplant my shared pointer here, so I did some modification.
+
+template<class T>
+class SharedPtr {
+private:
+    T *ptr = nullptr;
+    std::size_t *count = nullptr;
+public:
+    SharedPtr() : ptr(nullptr), count(nullptr) {};
+
+    explicit SharedPtr(T *input) : ptr(input) {
+        if (ptr != nullptr) {
+            count = new std::size_t;
+            *count = 1;
+        }
+    };
+
+    SharedPtr(SharedPtr const &input) {
+        ptr = input.ptr;
+        count = input.count;
+        if (count != nullptr && this != &input)(*count)++;
+    }
+
+    ~SharedPtr() {
+        reset();
+    }
+
+    operator bool() const {
+        if (ptr == nullptr) return false;
+        else return true;
+    }
+
+    SharedPtr &operator=(SharedPtr const &input) {
+        if (this != &input) {
+            reset();
+            ptr = input.ptr;
+            count = input.count;
+            if (count != nullptr) (*count)++;
+        }
+        return *this;
+    }
+
+    T *get() const {
+        return ptr;
+    }
+
+    T &operator*() {
+        return *ptr;
+    }
+
+    T *operator->() {
+        return ptr;
+    }
+
+    void reset() {
+        if (ptr != nullptr && *count > 1) (*count)--;
+        else {
+            delete ptr;
+            delete count;
+        }
+        ptr = nullptr;
+        count = nullptr;
+    }
+
+    void reset(T *input) {
+        if (ptr == input) return;
+        reset();
+        ptr = input;
+        if (ptr != nullptr) {
+            count = new std::size_t;
+            *count = 1;
+        }
+    }
+
+    std::size_t use_count() const {
+        if (ptr == nullptr) return 0;
+        else return *count;
+    }
+};
+
+template<typename T, typename ...Args>
+SharedPtr<T> make_shared(Args &&...args) {
+    return std::move(SharedPtr<T>{new T(std::forward<Args>(args)...)});
+}
+
 #endif
